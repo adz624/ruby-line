e269bae064c142d0ec829d5995621b0576c1b11c
eddie <eddie@visionbundles.com>
2015-03-05 04:57:52 +0800

    add comments and verify token

--- app.rb
+++ app.rb
@@ -7,9 +7,13 @@ require 'json'
 require 'open-uri'
 require 'openssl'
 require './helper'
+require 'net/http'
 
 
+email = ''
+password = ''
 
+# ====================================================
 LINE_DOMAIN = "http://gd2.line.naver.jp"
 LINE_HTTP_URL          = LINE_DOMAIN + "/api/v4/TalkService.do"
 LINE_HTTP_IN_URL       = LINE_DOMAIN + "/P4"
@@ -20,9 +24,6 @@ ip          = "127.0.0.1"
 version     = "3.7.0"
 com_name    = "eddie"
 revision    = 0
-
-email = ''
-password = ''
 
 json = JSON.parse(open(LINE_SESSION_LINE_URL).read)
 session_key = json['session_key']
@@ -41,6 +42,7 @@ pub_key = OpenSSL::PKey::RSA.new
 pub_key.n, pub_key.e = n.to_i(16), e.to_i(16)
 
 crypto = Digest.hexencode(pub_key.public_encrypt(message))
+
 # ==================
 os_version = "10.9.4-MAVERICKS-x64"
 user_agent = "DESKTOP:MAC:#{os_version}(#{version})"
@@ -53,8 +55,39 @@ transport.add_headers(headers)
 protocol = Thrift::CompactProtocol.new(transport)
 client = Line::TalkService::Client.new(protocol)
 begin
+  # 1. 第一次登入
   msg = client.loginWithIdentityCredentialForCertificate(Line::IdentityProvider::LINE, email, password, true, ip, com_name, "")
-  puts msg.inspect
+  puts "please input pin code in your devise: #{msg.pinCode}"
+  
+  # 2. blocking request 等待輸入 pin code
+  uri = URI(LINE_CERTIFICATE_URL)
+  req = Net::HTTP::Get.new(uri)
+  req['X-Line-Access'] = msg.verifier
+  Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+  
+
+  # 3. 取得 cert & authToken
+  auth_info = client.loginWithVerifierForCertificate(msg.verifier)
+  transport.add_headers("X-Line-Access" => auth_info.authToken)
+
+  # 4. 使用 auth token 重新登入 (back to step 1.)
+  client.loginWithIdentityCredentialForCertificate(Line::IdentityProvider::LINE, email, password, true, ip, com_name, "")
+  
+
+  # === 已取得權限
+
+  # a. 得到 revision
+  revision = client.getLastOpRevision()
+  puts "revision: #{revision}"
+
+  # b. 得到 profile information
+  profile = client.getProfile()
+  puts "LineID: #{profile.userid}, phone: #{profile.phone}, displayName: #{profile.displayName}, regionCode: #{profile.regionCode}"
+
+  # c. 得到 contact ids
+  contact_ids = client.getAllContactIds()
+  puts contact_ids.inspect
+
 rescue Exception => e
   puts e.inspect
 end
